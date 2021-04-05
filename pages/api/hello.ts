0ba3e88705e102b2bdd346e59d29f71f27d02cfe
rzbyn <12786693+rzbyn@users.noreply.github.com>
2021-04-05 20:17:12 +0700

    build: Convert js files to ts

---
+++ pages/api/hello.ts
@@ -0,0 +1,6 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default (req: NextApiRequest, res: NextApiResponse): void => {
+  return res.status(200).json({ name: 'John Doe' });
+};
