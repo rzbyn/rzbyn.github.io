4f859836b4f48f75a3596bb66b53ef05b9867d6a
abe <abe@rzbyn.com>
2025-05-09 01:15:07 +0700

    Better navbar layout

--- src/app/components/Navbar/index.tsx
+++ src/app/components/Navbar/index.tsx
@@ -4,9 +4,7 @@ import { JSX } from "react"
 function Navbar(): JSX.Element {
   return (
     <nav>
-      <Link href="/">About</Link>
-      <Link href="/writing">Wrinting</Link>
-      <Link href="/misc">Misc</Link>
+      <Link href="/">About</Link> / <Link href="/writing">Wrinting</Link> / <Link href="/misc">Misc</Link>
     </nav>
   )
 }
