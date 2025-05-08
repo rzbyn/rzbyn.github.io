cc7f37a7c09775cc95a8b19eddb83b5706482052
abe <abe@rzbyn.com>
2025-05-09 01:33:20 +0700

    Add spacing to footer

--- src/app/components/Footer/index.tsx
+++ src/app/components/Footer/index.tsx
@@ -2,9 +2,11 @@ import { JSX } from "react";
 
 function Footer(): JSX.Element {
   return (
-    <footer>
-      &#169; 2025 Reza Bayuni.
+    <footer className="flex justify-between">
       <div>
+        &#169; 2025 Reza Bayuni.
+      </div>
+      <div className="flex space-x-2">
         <a href="mailto:hi@rzbyn.com" target="_blank" rel="noopener noreferrer">Email</a>
         <a href="https://www.github.com/rzbyn" target="_blank" rel="noopener norefferer">Github</a>
         <a href="https://www.linkedin.com/in/rezabayuni" target="_blank" rel="noopener norefferer">LinkedIn</a>
