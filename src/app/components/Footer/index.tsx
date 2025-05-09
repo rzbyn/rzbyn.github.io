0da23fa67b403d9f664f484d47915e448c7a500d
abe <abe@rzbyn.com>
2025-05-09 14:12:38 +0700

    Use loop to render socials at footer

--- src/app/components/Footer/index.tsx
+++ src/app/components/Footer/index.tsx
@@ -1,16 +1,48 @@
+import Image from "next/image";
 import { JSX } from "react";
 
 function Footer(): JSX.Element {
+  const iconsPath = "/assets/icons/"
+  const socials = [
+    {
+      href: "mailto:hi@rzbyn.com",
+      src: iconsPath + "email.svg",
+      alt: "email logo",
+    },
+    {
+      href: "https://www.github.com/rzbyn",
+      src: iconsPath + "github.svg",
+      alt: "github logo",
+    },
+    {
+      href: "https://www.linkedin.com/in/rezabayuni",
+      src: iconsPath + "linkedin.svg",
+      alt: "linkedin logo",
+    },
+  ];
   return (
     <footer className="flex justify-between">
       <div>
         &#169; 2025 Reza Bayuni.
       </div>
-      <div className="flex space-x-2">
-        <a href="mailto:hi@rzbyn.com" target="_blank" rel="noopener noreferrer">Email</a>
-        <a href="https://www.github.com/rzbyn" target="_blank" rel="noopener norefferer">Github</a>
-        <a href="https://www.linkedin.com/in/rezabayuni" target="_blank" rel="noopener norefferer">LinkedIn</a>
-      </div>
+      <ul className="flex gap-2">
+        {
+          socials.map((social, index) => {
+            return (
+              <li key={index}>
+                <a href={social.href} target="_blank" rel="noopener noreferrer">
+                  <Image
+                    src={social.src}
+                    alt={social.alt}
+                    width={20}
+                    height={20}
+                  />
+                </a>
+              </li>
+            )
+          })
+        }
+      </ul>
     </footer>
   )
 }
