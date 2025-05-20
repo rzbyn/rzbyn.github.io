5709798681272cfde87a992744c51adf5cd5497c
abe <abe@rzbyn.com>
2025-05-21 02:06:11 +0700

    Use `&apos;` to escape character

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,13 +6,13 @@ export default function Home(): JSX.Element {
       <h1>About</h1>
       <div className="flex flex-col gap-2">
         <p>
-          I'm a developer hailing from Indonesia. I lead Backend Engineering team at <a href="https://datasintesa.id/" target="_blank" rel="noopener noreferrer" className="text-blue-500 underline">Datasintesa</a>.
+          I&apos;m a developer hailing from Indonesia. I lead Backend Engineering team at <a href="https://datasintesa.id/" target="_blank" rel="noopener noreferrer" className="text-blue-500 underline">Datasintesa</a>.
         </p>
         <p>
           I try to share or write things I found interesting to me or something I do.
         </p>
         <p>
-          When I'm not coding, you can often finde me in the kitchen 🍳.
+          When I&apos;m not coding, you can often finde me in the kitchen 🍳.
         </p>
       </div>
     </section>
