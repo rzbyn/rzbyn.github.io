b97b832cddb1e1383cdd576ac322d3383a8de6c8
abe <abe@rzbyn.com>
2025-06-03 19:10:10 +0700

    Add esater egg in not-found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,13 +1,15 @@
 'use client'
 
-import { JSX, useEffect, useState } from "react";
+import { notFoundEasters } from "@/app/_lib/consts/not-found-easters";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
+import { JSX, useEffect, useState } from "react";
 
 function NotFound(): JSX.Element {
   const router = useRouter()
   const [countdown, setCountdown] = useState(30)
-  const timeOutDelay = 30000;
+  const timeOutDelay = 30000
+  const [{ icon, aria_label, text }, setRandomMessage] = useState({ icon: "", aria_label: '', text: '' })
 
   useEffect(() => {
     const redirectTimer = setTimeout(() => {
@@ -22,14 +24,22 @@ function NotFound(): JSX.Element {
         return prevCountdown - 1
       })
     }, 1000)
+    setRandomMessage(notFoundEasters[Math.floor(Math.random() * notFoundEasters.length)])
     return () => { clearTimeout(redirectTimer); clearInterval(countdownInterval) }
   }, [router])
 
   return (
-    <section>
+    <>
       <h1>404 Page Not Found</h1>
-      <p>What are you looking for? Go to <Link href="/" className="text-blue-500">homepage</Link> [{countdown}]</p>
-    </section>
+      <section className='py-12 text-center'>
+        <p className='my-10'>
+          <span role='img' aria-label={aria_label} className='text-6xl'>{icon}</span>
+        </p>
+        <p className='mb-36'>{text}</p>
+        <p>What are you looking for? Go to <Link href="/" className="text-blue-500">homepage</Link></p>
+        <p>[{countdown}]</p>
+      </section>
+    </>
   )
 }
 
