854ce9287c8fedb5c58ec21191325a50070b5ac6
abe <abe@rzbyn.com>
2025-06-14 13:47:39 +0700

    Rename const and split `useEffect`
    Use clear meaning name for const.
    Split long `useEffect` and enforce separation of concern.

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,20 +1,30 @@
 'use client'
 
-import { notFoundEasters } from "@/app/_lib/consts/not-found-easters";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { JSX, useEffect, useState } from "react";
+import { errorMessages } from "./_lib/consts/error-messages";
 
 function NotFound(): JSX.Element {
   const router = useRouter()
   const [countdown, setCountdown] = useState(30)
   const timeOutDelay = 30000
-  const [{ icon, aria_label, text }, setRandomMessage] = useState({ icon: "", aria_label: '', text: '' })
+  const [{ icon, aria_label, message }, setMessage] = useState({ icon: "", aria_label: "", message: "" })
+
+  useEffect(() => {
+    const message = errorMessages[Math.floor(Math.random() * errorMessages.length)]
+    setMessage(message)
+    return
+  }, [])
 
   useEffect(() => {
     const redirectTimer = setTimeout(() => {
       router.push('/')
     }, timeOutDelay)
+    return () => { clearTimeout(redirectTimer) }
+  }, [router])
+
+  useEffect(() => {
     const countdownInterval = setInterval(() => {
       setCountdown((prevCountdown) => {
         if (prevCountdown <= 1) {
@@ -24,9 +34,8 @@ function NotFound(): JSX.Element {
         return prevCountdown - 1
       })
     }, 1000)
-    setRandomMessage(notFoundEasters[Math.floor(Math.random() * notFoundEasters.length)])
-    return () => { clearTimeout(redirectTimer); clearInterval(countdownInterval) }
-  }, [router])
+    return () => { clearInterval(countdownInterval) }
+  })
 
   return (
     <>
@@ -35,7 +44,7 @@ function NotFound(): JSX.Element {
         <p className='my-10'>
           <span role='img' aria-label={aria_label} className='text-6xl'>{icon}</span>
         </p>
-        <p className='mb-36'>{text}</p>
+        <p className='mb-36'>{message}</p>
         <p>What are you looking for? Go to <Link href="/" className="text-blue-500">homepage</Link></p>
         <p>[{countdown}]</p>
       </section>
