b7bbb5400dd70eb4125c8dae7e2c6b5d628e6ca4
abe <abe@rzbyn.com>
2025-08-10 02:49:05 +0700

    Fix typo at homepage paragraph

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home(): JSX.Element {
           I&apos;m a developer hailing from Indonesia. I lead Backend Engineering team at <a href="https://datasintesa.id/" target="_blank" rel="noopener noreferrer" className="text-blue-500 underline">Datasintesa</a>.
         </p>
         <p>
-          I try to write things I do to or something I found interesting.
+          I try to write things I do or something I found interesting.
         </p>
         <p>
           When I&apos;m not coding, you can often find me in the kitchen 🍳.
