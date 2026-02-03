f37feddcb1877fa7353d3f27a75c98412f5b5e9b
abe <abe@rzbyn.com>
2026-02-03 17:37:08 +0700

    chore: configure linter
    Add Prettier configurations.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,11 @@
-import nextVitals from "eslint-config-next/core-web-vitals";
-import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 const eslintConfig = defineConfig([
   nextVitals,
-  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
+  eslintConfigPrettier,
+  globalIgnores(['.next/**', 'out/**', 'build/**', 'next-env.d.ts']),
   {
     languageOptions: {
       ecmaVersion: 2022,
@@ -11,23 +13,24 @@ const eslintConfig = defineConfig([
         jsx: true,
       },
     },
+    files: ['src/**/*.{ts,tsx}'],
     rules: {
-      semi: ["error", "always"],
-      "eol-last": ["error", "always"],
+      semi: ['error', 'always'],
+      'eol-last': ['error', 'always'],
       quotes: [
-        "error",
-        "single",
+        'error',
+        'single',
         { avoidEscape: true, allowTemplateLiterals: true },
       ],
-      "no-trailing-spaces": "error",
-      indent: ["error", 2, { SwitchCase: 1 }],
-      "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
-      "line-comment-position": ["error", { position: "above" }],
-      "react/display-name": "off",
-      "react/jsx-key": "error",
-      "react-hooks/rules-of-hooks": "error",
-      "react-hooks/exhaustive-deps": "warn",
-      "@next/next/no-img-element": "warn",
+      'no-trailing-spaces': 'error',
+      indent: ['error', 2, { SwitchCase: 1 }],
+      'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      'line-comment-position': ['error', { position: 'above' }],
+      'react/display-name': 'off',
+      'react/jsx-key': 'error',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      '@next/next/no-img-element': 'warn',
     },
   },
 ]);
