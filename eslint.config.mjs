8f4158350cb7eaf4b197e79f2b519a25e6eb08a3
abe <abe@rzbyn.com>
2026-02-03 17:02:46 +0700

    chore: update configs
    Update eslint, next, and typescript configurations.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,35 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import { defineConfig, globalIgnores } from "eslint/config";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+const eslintConfig = defineConfig([
+  nextVitals,
+  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      parserOptions: {
+        jsx: true,
+      },
+    },
+    rules: {
+      semi: ["error", "always"],
+      "eol-last": ["error", "always"],
+      quotes: [
+        "error",
+        "single",
+        { avoidEscape: true, allowTemplateLiterals: true },
+      ],
+      "no-trailing-spaces": "error",
+      indent: ["error", 2, { SwitchCase: 1 }],
+      "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+      "line-comment-position": ["error", { position: "above" }],
+      "react/display-name": "off",
+      "react/jsx-key": "error",
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+      "@next/next/no-img-element": "warn",
+    },
+  },
+]);
 
 export default eslintConfig;
