1cc3f365bc054ca007c66510261ffb1ddd24d1cf
abe <abe@rzbyn.com>
2026-02-04 17:53:38 +0700

    feat(Footer): use dynamic year in footer copyright
    Replace hard-code year with `new Date.getFullYear()` in the Footer copyright
    so the displayed year update automatically.

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,9 +3,10 @@ import { socialItems } from '@consts/social-items';
 import type { JSX } from 'react';
 
 function Footer(): JSX.Element {
+  const year = new Date().getFullYear();
   return (
     <footer className="flex justify-between">
-      <div>&#169; 2025 Reza Bayuni.</div>
+      <div>&#169; {year} Reza Bayuni.</div>
       <ul className="flex gap-2">
         {socialItems.map((socialItem, index: number): JSX.Element => {
           return (
